e1c4224f88ba8777840c7ea895d75e647f6ca7b6
Yurii <4114114jc@gmail.com>
2022-11-10 19:53:32 +0200

    'update'

---
+++ src/js/03-promises.js
@@ -0,0 +1,41 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+form.addEventListener('click', onSubmit);
+
+function onSubmit(event) {
+  event.preventDefault();
+  const { delay, step, amount } = event.currentTarget;
+
+  let delayRef = +delay.value;
+  let stepRef = +step.value;
+  let amountRef = +amount.value;
+
+  for (position = 1; position <= amountRef; position++) {
+    createPromise(position, delayRef);
+    delayRef += stepRef;
+  }
+}
+
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+
+  promise
+    .then(({ position, delay }) => {
+      Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+      console.log(`Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+      console.log(`Rejected promise ${position} in ${delay}ms`);
+    });
+}
